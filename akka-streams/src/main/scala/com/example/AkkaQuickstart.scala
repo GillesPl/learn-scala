31757bdf4dc8a0a0b9befa7128b20fa5b31e45eb
gilles platteeuw <platteeuw.gilles@gmail.com>
2018-12-28 13:43:40 +0100

    update

--- akka-streams/src/main/scala/com/example/AkkaQuickstart.scala
+++ akka-streams/src/main/scala/com/example/AkkaQuickstart.scala
@@ -2,6 +2,7 @@
 package com.example
 
 import java.nio.file.Paths
+import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
 import akka.stream._
@@ -9,8 +10,10 @@ import akka.stream.scaladsl._
 import akka.util.ByteString
 import akka.{Done, NotUsed}
 import com.example.FirstSteps.{done, system}
+import com.example.firstSteps2.{result, system}
 
 import scala.concurrent._
+import scala.concurrent.duration.FiniteDuration
 
 
 // Main method = starting point of application
@@ -66,58 +69,145 @@ object firstSteps2 extends App {
   })
 }
 
-object firstStepsTweet extends App {
+object firstStepsEmployees extends App {
 
-  // author of a tweet
-  final case class Author(handle: String)
+  final case class Worker(name: String)
 
-  // acutal tweet hashtag
-  final case class Hashtag(name: String)
+  final case class JobDescription(name: String, proffesion: String)
 
-  // tweet class
-  final case class Tweet(author: Author, timestamp: Long, body: String) {
-    // function that collects all the hashtags in the body and returns it as a set of Hashtags
-    def hashtags: Set[Hashtag] = body.split(" ").collect {
-      // returnes the tag without the hash (#test => test)
-      case t if t.startsWith("#") ⇒ Hashtag(t.replaceAll("[^#\\w]", ""))
+  final case class Employee(worker: Worker, startedOn: Long, description: String) {
+    // function that collects all the proffessions in the desc and returns it as a set of professions
+    // this is based on the example just as an easy thing to work with
+    def getJobs: Set[JobDescription] = description.split(" ").collect {
+      // returnes the proffesions without the @ (@test => test)
+      case t if t.startsWith("@") ⇒ JobDescription(worker.name, t)
     }.toSet
   }
 
   // hier gaan we een source van tweets maken, dit is de blueprint van onze stream
   // later gaan we de stream uitvoeren maar die nog voor en tijdens de flow bewerken
-  val tweets: Source[Tweet, NotUsed] = Source(
-    Tweet(Author("Michallis"), System.currentTimeMillis, "#T1T rocks!") ::
-      Tweet(Author("Guillaume"), System.currentTimeMillis, "#T1T!") ::
-      Tweet(Author("Gilles"), System.currentTimeMillis, "#T1T !") ::
-      Tweet(Author("Jonas"), System.currentTimeMillis, "#T1T !") ::
-      Tweet(Author("Guillaume"), System.currentTimeMillis, "#T1T on the rocks!") ::
-      Tweet(Author("Guillaume"), System.currentTimeMillis, "wow #T1T !") ::
-      Tweet(Author("Guillaume"), System.currentTimeMillis, "#T1T rocks!") ::
-      Tweet(Author("Michallis"), System.currentTimeMillis, "#malaka rock!") ::
-      Tweet(Author("Michallis"), System.currentTimeMillis, "#Gamiseta rock!") ::
-      Tweet(Author("poopooo"), System.currentTimeMillis, "we compared #Tjoepkes to #Tjapkes!") ::
+  val employees: Source[Employee, NotUsed] = Source(
+    Employee(Worker("Michallis"), System.currentTimeMillis, "@Architect and @CTO and @CEO and @TheOneWithTheMagic for Trust1Team") ::
+      Employee(Worker("Guillaume"), System.currentTimeMillis, "@MediorDeveloper for Trust1Team") ::
+      Employee(Worker("Gilles"), System.currentTimeMillis, "@JuniorDeveloper for Trust1Team") ::
+      Employee(Worker("Jonas"), System.currentTimeMillis, "@SeniorDeveloper for Trust1Team") ::
+      Employee(Worker("Michallis"), System.currentTimeMillis, "@Architect and @CTO and @CEO and @TheOneWithTheMagic for Trust1Team") ::
+      Employee(Worker("Jonas"), System.currentTimeMillis, "@SeniorDeveloper for Trust1Team") ::
       Nil)
 
-  implicit val system = ActorSystem("reactive-tweets")
+  implicit val system = ActorSystem("super-employees")
+  implicit val ec = system.dispatcher
   implicit val materializer = ActorMaterializer()
 
-  tweets
-    .map(_.hashtags) // Get all sets of hashtags ...
-    .reduce(_ ++ _) // ... and reduce them to a single set, removing duplicates across all tweets
+  employees
+    .map(_.getJobs) // haal alle descriptions op
+    .reduce(_ ++ _) // breng ze naar 1 set, duplicaten als ze er in zitten halen we er uit
     /**
       * Transform each input element into an `Iterable` of output elements that is
       * then flattened into the output stream.
       *
       * A stream is given as a stream of sequence of elements, but a stream of elements needed instead, streaming all the nested elements inside the sequences separately.
-      * he mapConcat operation can be used to implement a one-to-many transformation of elements using a mapper function in the form of In => immutable.Seq[Out] . In this case we want to map a Seq of elements to the elements in the collection itself, so we can call mapConcat(identity) .
+      * he mapConcat operation can be used to implement a one-to-many transformation of elements using a mapper function in the form of In => immutable.Seq[Out] .
+      * In this case we want to map a Seq of elements to the elements in the collection itself, so we can call mapConcat(identity) .
       * voorbeeld:
       * val myData: Source[List[Message], NotUsed] = someDataSource
       * val flattened: Source[Message, NotUsed] = myData.mapConcat(identity)
       *
       * in ons geval hebben we een stream van tweets maar willen we een stream van hashtags dus gebruiken we de mapconcat om die stream te flattenen naar een hashtag stream
-    */
+      */
     .mapConcat(identity)
-    .map(_.name.toUpperCase) // Convert all hashtags to upper case, simpele map over elk element in de flow
-    .runWith(Sink.foreach(println)) // Attach the Flow to a Sink that will finally print the hashtags
+    .map(jobs => JobDescription(jobs.name, jobs.proffesion.toUpperCase())) // Convert all hashtags to upper case, simpele map over elk element in de flow
+    .runWith(Sink.foreach(job => println("geia sou " + job.name + " " + job.proffesion))) // Attach the Flow to a Sink that will finally print the hashtags
+    .onComplete(_ => {
+    println("done and terminating")
+    system.terminate()
+  })
 
+}
+
+
+object Reuseability extends App {
+  implicit val system = ActorSystem("QuickStart")
+  implicit val materializer = ActorMaterializer()
+
+  val source: Source[Int, NotUsed] = Source(1 to 100)
+  val factorials = source.scan(BigInt(1))((acc, next) ⇒ acc * next)
+
+  // runwith returned een materialized view van die flow (source naar sink)
+  //FLOW1
+  factorials.map(_.toString).runWith(lineSink("factorial2.txt"))
+
+  // FLOW2
+  factorials
+    .zipWith(Source(0 to 100))((num, idx) ⇒ s"$idx! = $num")
+    // we gaan de stream throttelen naar 1 element per seconde
+    /** de throttle operator gaat zeggen dat hij maar x aantal element per seconde kan verwerken daardoor gaat die de upstream sources een melding geven dat hij maar zoveel aan kan
+      * en gaat hij automatisch backpressure toevoegen aan de upstream
+      */
+    .throttle(1, FiniteDuration(1, TimeUnit.SECONDS))
+    .runForeach(println)
+
+  // FLOW3
+  val result: Future[IOResult] =
+    factorials
+      .map(num ⇒ ByteString(s"$num\n"))
+      .runWith(FileIO.toPath(Paths.get("factorials.txt")))
+
+  // default dispatcher/execution context
+  implicit val ec = system.dispatcher
+  result.onComplete(_ ⇒ {
+    println("done and terminating")
+    system.terminate()
+  })
+
+  // de lineSink gaat een sink aanvaarden van strings en gaat een future[IOResult maken als een auxillary]
+  def lineSink(filename: String): Sink[String, Future[IOResult]] =
+  // input van de stream is een string value en we returnen de flow
+  //Flow[In,Out,M2]
+    Flow[String]
+      // die string value omzetten naar een bytestring
+      .map(s ⇒ ByteString(s + "\n"))
+      // keep right zorgt uervoor dat we de rechtse materialized value behoden dus in ons geval de Future[IOResult], waar de aangepaste file dus in zit
+      // hier maken we een nieuwe Sink[In, M3]
+      .toMat(FileIO.toPath(Paths.get(filename)))(Keep.right)
+}
+
+
+object ReactiveExample extends App {
+
+  final case class Worker(name: String)
+
+  final case class JobDescription(name: String, proffesion: String)
+
+  final case class Employee(worker: Worker, startedOn: Long, description: String) {
+
+    def getJobs: Set[JobDescription] = description.split(" ").collect {
+      case t if t.startsWith("@") ⇒ JobDescription(worker.name, t)
+    }.toSet
+  }
+
+  //Source[Out,M1]
+  val employees: Source[Employee, NotUsed] = Source(
+    Employee(Worker("Michallis"), System.currentTimeMillis, "@Architect and @CTO and @CEO and @TheOneWithTheMagic for Trust1Team") ::
+      Employee(Worker("Guillaume"), System.currentTimeMillis, "@Developer for Trust1Team") ::
+      Employee(Worker("Gilles"), System.currentTimeMillis, "@Developer for Trust1Team") ::
+      Employee(Worker("Jonas"), System.currentTimeMillis, "@Developer for Trust1Team") ::
+      Employee(Worker("Michallis"), System.currentTimeMillis, "@Architect and @CTO and @CEO and @TheOneWithTheMagic for Trust1Team") ::
+      Employee(Worker("Jonas"), System.currentTimeMillis, "@Developer for Trust1Team") ::
+      Nil)
+
+  implicit val system = ActorSystem("super-employees")
+  implicit val ec = system.dispatcher
+  implicit val materializer = ActorMaterializer()
+
+  employees
+    .map(_.getJobs)
+    .reduce(_ ++ _)
+    .mapConcat(identity)
+    .map(jobs => JobDescription(jobs.name, jobs.proffesion.toUpperCase()))
+    .runWith(Sink.foreach(job => println("geia sou " + job.name + " " + job.proffesion))) // hier is de Sink[In, M3]
+    .onComplete(_ => {
+    println("done and terminating")
+    system.terminate()
+  })
 }
